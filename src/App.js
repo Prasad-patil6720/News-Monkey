d13e35e87e66ff54b5cae7179215ff15d24fcc0c
Prasad-patil6720 <prasadpatil6720@gmail.com>
2021-10-16 17:57:51 +0530

    changes will be saved

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   c='john';
   render() {
     return (
       <div>
-        hello my first class base component {this.c}
+       <Navbar/>
+       <News/>
       </div>
     )
   }
